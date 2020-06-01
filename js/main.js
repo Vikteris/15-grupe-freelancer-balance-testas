0116de4c04e740059fa01d86cea807baaa83e8f9
Viktoras <vikter@inbox.lt>
2020-06-01 14:14:51 +0300

    3 užduotis

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+//3 užduotis//
+const menesiai = ["sausis", "vasris" , "kovas", "balandis", "geguze" , "birzelis" , "liepa" , "rugpjutis" , 'rugesjis', "spalis", "lapkritis", "gruodis"];
+
+console.log(menesiai);
+
